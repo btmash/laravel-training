5d216081a1236fe1838bfa90904b81ff8f4459f4
Ashok Modi <ashok.modi@card.com>
2017-08-05 12:56:11 -0700

    Update Training model with fields and relationship

--- app/Training.php
+++ app/Training.php
@@ -2,9 +2,21 @@
 
 namespace App;
 
+use App\User;
+
 use Illuminate\Database\Eloquent\Model;
 
-class Training extends Model
-{
-    //
+class Training extends Model {
+  protected $fillable = [
+    'title',
+    'link',
+    'location',
+    'body',
+    'date_from',
+    'date_to',
+  ];
+
+  public function author() {
+    return $this->belongsTo('User', 'user_id');
+  }
 }
